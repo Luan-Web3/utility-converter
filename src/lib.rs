0ca87a2546fab00572012873aa9041df2e30f9bc
Luan-Web3 <luansilvalemes.web3@gmail.com>
2024-12-11 16:25:02 -0300

    add: solving #6

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod temperature;
 mod weight;
+mod distance;
 
 pub mod converter {
     pub use crate::temperature::temperature_conversation::*;
     pub use crate::weight::weight_conversation::*;
+    pub use crate::distance::distance_conversation::*;
 }
